1a27bf4dbcc01ffa5707478a23adaf3b900e3f07
SEUNGWOO AN <asw0899@naver.com>
2021-02-16 23:03:16 +0900

    appbar21.02.16

--- chat_demo/lib/main.dart
+++ chat_demo/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,12 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          child: Text("아무거나"),
-          color: Colors.red,
-        ),
-      ),
+      home: LoginPage(),
+
     );
   }
 }
